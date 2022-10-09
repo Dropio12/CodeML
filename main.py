eef9a92f2884e55c07a12abf7dd2696588418963
anass <anass.elhallaoui@gmail.com>
2022-10-08 20:55:45 -0400

    Let's go 4th Place For A medium challenge for our 1st hackathon! Thanks codeMl!

--- main.py
+++ main.py
@@ -1,55 +1,34 @@
-# create a model that can detect the language of a text
-# usage: python3 main.py [path_to_data]
-
-import sys
-import os
-import numpy as np
-import pandas as pd
+﻿import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.svm import LinearSVC
 from sklearn.tree import DecisionTreeClassifier
 
-def get_data(path: str):
-    """Get data from a csv file
-    Args:
-        path (str): path to the csv file
-    Returns:
-        pd.DataFrame: dataframe containing the data
-    """
-    df = pd.read_csv(path, index_col=0)
-    return df
-
 def main():
-    """Main function
-    """
-    # get the data
-    path = sys.argv[1]
-    df = get_data(path:)
+    # transfère du doc csv
+    df = pd.read_csv('language-identification-datasets.csv')
 
-    # split the data
-    X = df['text']
-    y = df['language']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # division en 2 array pour text et langue
+    X = df['Text']
+    y = df['Language']
+    z = df['ID']
+    X_train = X[0:24326]
+    X_test = X[24300:31014]
+    y_train = y[0:24326]
+    y_test = y[24300:31014]
+    z_test = z[24300:31014]
 
-    # create the model
+    # creation model
     model = Pipeline([
         ('vectorizer', CountVectorizer()),
         ('classifier', DecisionTreeClassifier())
     ])
 
-    # train the model
+    # apprentissage
     model.fit(X_train, y_train)
 
-    # evaluate the model
+    # évalutation model
     y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    print(f'Accuracy: {acc}')
+    pd.DataFrame(y_pred, z_test).to_csv('ModèleFinale.csv')
 
 if __name__ == "__main__":
-    main()
-
+  main()
